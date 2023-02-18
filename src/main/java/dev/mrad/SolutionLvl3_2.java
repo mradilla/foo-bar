55df6b1f1a03fda33b4be3fd294fd5353d29f99f
Martha Radilla <martharadillar@gmail.com>
2023-02-18 00:06:56 -0600

    level 3, challenge 2

---
+++ src/main/java/dev/mrad/SolutionLvl3_2.java
@@ -0,0 +1,31 @@
+package dev.mrad;
+
+import java.math.BigInteger;
+
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+
+public class SolutionLvl3_2 {
+
+    public static int solution(String x) {
+        BigInteger n = new BigInteger(x);
+        BigInteger THREE = BigInteger.valueOf(3);
+        BigInteger FOUR = BigInteger.valueOf(4);
+        int count = 0;
+        while (n.bitLength() > 1) {
+            if (!n.testBit(0)) {
+                n = n.shiftRight(1);
+            } else {
+                BigInteger plusOne = n.add(ONE);
+                if (n.compareTo(THREE) > 0 && plusOne.mod(FOUR).equals(ZERO)) {
+                    n = plusOne;
+                } else {
+                    n = n.subtract(ONE);
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+
+}
